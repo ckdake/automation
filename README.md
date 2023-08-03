c9e9e9cfde7ec332bb3aef84732726b0fd9619ee
Chris Kelly <ckdake@ckdake.com>
2023-08-03 18:13:55 +0000

    Adds dynamodb and infracost

--- README.md
+++ README.md
@@ -4,6 +4,11 @@
 
 This requires env vars with root credentials for the root account.
 
+```
+export AWS_ACCESS_KEY_ID=
+export AWS_SECRET_ACCESS_KEY=
+```
+
 ### TODO
 
 - [] linters and formatters etc
@@ -11,4 +16,5 @@ This requires env vars with root credentials for the root account.
 - [] import everything in root account, test1 account, test2 account
 - [] get some securityhub things passing
 - [] setup github actions for terraform plan, terraform apply
+- [] terraform plugin caching
 - [] lots more
--- tenants/management/terraform.tf
+++ tenants/management/terraform.tf
@@ -4,3 +4,25 @@ module "terraform_state_bucket" {
     logging_bucket_name = module.terraform_logging_bucket.id
     kms_key_arn = aws_kms_key.management.arn
 }
+
+resource "aws_dynamodb_table" "terraform_lock" {
+  name = "terraform-lock"
+
+  billing_mode = "PAY_PER_REQUEST"
+
+  hash_key = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+
+  point_in_time_recovery {
+    enabled = true
+  }
+
+  server_side_encryption {
+    enabled     = true
+    kms_key_arn = aws_kms_key.management.arn
+  }
+}
