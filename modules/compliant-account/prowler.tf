0370c0561f6b84a5a444de7ee46809cfcaf286fd
Chris Kelly <ckdake@ckdake.com>
2023-08-04 13:25:02 +0000

    Enables prowler in securityhub

---
+++ modules/compliant-account/prowler.tf
@@ -0,0 +1,7 @@
+# For now, prowler is run by hand as a user via the administrator role. In the future,
+# this should be run on a schedule, by a prowler role with dedicated read-only
+# permissions and write permissions to Security. See the SecurityAudit policy.
+
+resource "aws_securityhub_product_subscription" "prowler" {
+  product_arn = "arn:aws:securityhub:us-east-1::product/prowler/prowler"
+}
