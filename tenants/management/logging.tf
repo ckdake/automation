504a946879b9a457cae8865d31afce498165b3a0
Chris Kelly <ckdake@ckdake.com>
2023-12-12 03:28:14 +0000

    Adds more log perms

--- tenants/management/logging.tf
+++ tenants/management/logging.tf
@@ -12,6 +12,27 @@ module "s3_access_log_bucket" {
   bucket_name         = local.s3_access_log_bucket_name
   logging_bucket_name = local.s3_access_log_bucket_name
   kms_key_arn         = aws_kms_key.management.arn
+
+  additional_bucket_policy_statements = [
+    <<EOP
+        {
+            "Sid": "S3ServerAccessLogsPolicy",
+            "Effect": "Allow",
+            "Principal": {
+                "Service": "logging.s3.amazonaws.com"
+            },
+            "Action": [
+                "s3:PutObject"
+            ],
+            "Resource": "arn:aws:s3:::${local.s3_access_log_bucket_name}/*",
+            "Condition": {
+                "StringEquals": {
+                    "aws:SourceAccount": "${aws_organizations_account.management.id}"
+                }
+            }
+        }
+EOP
+  ]
 }
 
 module "aws_logs" {
@@ -22,4 +43,64 @@ module "aws_logs" {
 
   bucket_name         = local.aws_logs_bucket_name
   logging_bucket_name = local.s3_access_log_bucket_name
+
+  additional_bucket_policy_statements = [
+    <<EOP1
+        {
+            "Sid": "S3ServerAccessLogsPolicy",
+            "Effect": "Allow",
+            "Principal": {
+                "Service": "logging.s3.amazonaws.com"
+            },
+            "Action": [
+                "s3:PutObject"
+            ],
+            "Resource": "arn:aws:s3:::${local.aws_logs_bucket_name}/*",
+            "Condition": {
+                "StringEquals": {
+                    "aws:SourceAccount": "${aws_organizations_account.management.id}"
+                }
+            }
+        }
+EOP1
+    ,
+    <<EOP2
+        {
+            "Sid": "AWSLogDeliveryWrite",
+            "Effect": "Allow",
+            "Principal": {
+                "Service": "delivery.logs.amazonaws.com"
+            },
+            "Action": [
+                "s3:PutObject"
+            ],
+            "Resource": "arn:aws:s3:::${local.aws_logs_bucket_name}/*",
+            "Condition": {
+               "ArnLike": {
+                    "aws:SourceArn": "arn:aws:logs::${aws_organizations_account.management.id}:*"
+                }
+            }
+        }
+EOP2
+    ,
+    <<EOP3
+        {
+            "Sid": "AWSLogDeliveryCheck",
+            "Effect": "Allow",
+            "Principal": {
+                "Service": "delivery.logs.amazonaws.com"
+            },
+            "Action": [
+                "s3:GetBucketAcl",
+                "s3:ListBucket"
+            ],
+            "Resource": "arn:aws:s3:::${local.aws_logs_bucket_name}",
+            "Condition": {
+               "ArnLike": {
+                    "aws:SourceArn": "arn:aws:logs::${aws_organizations_account.management.id}:*"
+                }
+            }
+        }
+EOP3
+  ]
 }
